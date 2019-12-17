377e2881ab48ab2aed96605ecabeaaeaec3d76e8
Leon <leonbaraza@gmail.com>
2019-12-17 20:51:04 +0300

    Updated

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template, request, redirect, url_for
+from _ast import In
+from idlelib.idle_test.test_iomenu import S
+
+from flask import Flask, render_template, request, redirect, url_for, flash
 
 import pygal
 
@@ -15,11 +18,43 @@ db = SQLAlchemy(app)
 
 
 from models.inventories import Inventories
+from models.sales import Sales
 
 @app.before_first_request
 def create_tables():
     db.create_all()
 
+# @app.route('/test/<num1>/<num2>')
+# def test(num1,num2):
+#
+#     print(int(num1)+int(num2))
+#     return 'Yes'
+
+@app.route('/salepro/<int:id>', methods=['POST', 'GET'])
+def makeSales(id):
+    record = Inventories.fetch_one_record(id)
+    if record:
+        if request.method == 'POST':
+
+            quantity = request.form['quantity']
+            newStock = record.stock - int(quantity)
+
+            record.stock = newStock
+            db.session.commit()
+
+            sales = Sales(inv_id=id, quantity = quantity)
+            sales.add_records()
+
+            flash('You successfully made a sale', 'success')
+
+    return redirect(url_for('hello_world'))
+
+@app.route('/viewsales/<int:id>')
+def viewSales(id):
+
+    record = Inventories.fetch_one_record(id)
+
+    return render_template('viewsales.html', record=record)
 
 
 @app.route('/')
@@ -50,52 +85,86 @@ def add_inventory():
     # return 'Naibei'
     return redirect(url_for('hello_world'))
 
-#
-# @app.route('/dashboard')
-# def pie_chart():
-#
-#     conn = psycopg2.connect("dbname='sales_demo' user='postgres' host='localhost' password='Nancy22*'")
-#
-#     cur = conn.cursor()
-#
-#     cur.execute ("""SELECT to_char(to_timestamp (date_part('month', s.created_at)::text, 'MM'), 'Month'), sum(i.selling_price * s.quantity)
-# FROM public.inventories as i
-# join sales as s on s.inventory_id = i.id
-# group by EXTRACT(MONTH FROM s.created_at)
-# order by EXTRACT(MONTH FROM s.created_at) asc
-# """)
-#
-#     rows = cur.fetchall()
-#     #print(type(rows))
-#     x_axis=[]
-#     y_axis=[]
-#
-#     for each in rows:
-#         x_axis.append(each[0])
-#         y_axis.append(each[1])
-#     # print(x_axis)
-#     # print(y_axis)
-#         #print(each)
-#
-#
-#     # pie_chart = pygal.Pie(inner_radius=.4)
-#     # pie_chart.title = 'Browser usage in February 2012 (in %)'
-#     # pie_chart.add('IE', 19.5)
-#     # pie_chart.add('Firefox', 36.6)
-#     # pie_chart.add('Chrome', 36.3)
-#     # pie_chart.add('Safari', 4.5)
-#     # pie_chart.add('Opera', 2.3)
-#     # pie_data = pie_chart.render_data_uri()
-#     # return render_template('dashboard.html',pie_data=pie_data)
-#
-#     # graph = pygal.Line()
-#     # graph.title = 'Monthly Sales'
-#     # graph.x_labels = x_axis
-#     # graph.add('Total Sales', y_axis)
-#
-#     graph_data = graph.render_data_uri()
-#
-#     return render_template('dashboard.html', graph_data=graph_data)
+@app.route('/sale/<int:id>', methods=['POST','GET'])
+def makeSale(id):
+    rec = Inventories.fetch_one_record(id)
+
+    if rec:
+        if request.method == 'POST':
+            newStock = rec.stock - int(request.form['quantity'])
+            rec.stock = newStock
+            db.session.commit()
+        # saleRecord = Sales(inv_id=id, quantity=)
+    return redirect(url_for('hello_world'))
+
+
+@app.route('/delete')
+
+
+@app.route('/dashboard')
+def piechart():
+
+    conn = psycopg2.connect("dbname='sales_demo' user='postgres' host='localhost' password='Leon@1996' ")
+
+    ratios = [('Gentlemen', 5), ('Ladies',9)]
+    ratios[0][0]
+    # ratios1 = [{},{}]
+    pie_chart = pygal.Pie()
+    pie_chart.title = 'Browser usage in February 2012 (in %)'
+    pie_chart.add(ratios[0][0], ratios[0][1])
+    pie_chart.add(ratios[1][0], ratios[1][1])
+    # pie_chart.add('Chrome', 36.3)
+    # pie_chart.add('Safari', 4.5)
+    # pie_chart.add('Opera', 2.3)
+    pie_data = pie_chart.render_data_uri()
+
+    data = [
+        {'month': 'January', 'total': 22},
+        {'month': 'February', 'total': 27},
+        {'month': 'March', 'total': 23},
+        {'month': 'April', 'total': 20},
+        {'month': 'May', 'total': 12},
+        {'month': 'June', 'total': 32},
+        {'month': 'July', 'total': 42},
+        {'month': 'August', 'total': 72},
+        {'month': 'September', 'total': 52},
+        {'month': 'October', 'total': 42},
+        {'month': 'November', 'total': 92},
+        {'month': 'December', 'total': 102}
+    ]
+    cur = conn.cursor()
+
+    cur.execute("""SELECT (sum(i.selling_price * s.quatity)) as subtotal, EXTRACT(MONTH FROM s.created_at) as sales_month 
+    from sales as s 
+    join inventories as i on s.inv_id = i.id
+    GROUP BY sales_month
+    ORDER BY sales_month
+    """)
+
+    rows = cur.fetchall()
+    # print(type(rows))
+    months = []
+    total_sales = []
+
+    for each in rows:
+        months.append(each[1])
+        total_sales.append(each[0])
+
+    print(months)
+    print(total_sales)
+
+    graph = pygal.Line()
+    graph.title = '% Change Coolness of programming languages over time.'
+    graph.x_labels = months
+    graph.add('Total Sales', total_sales)
+    # graph.add('Java', [15, 45, 76, 80, 91, 95])
+    # graph.add('C++', [5, 51, 54, 102, 150, 201])
+    # graph.add('All others combined!', [5, 15, 21, 55, 92, 105])
+    graph_data = graph.render_data_uri()
+    # return render_template("graphing.html", graph_data=graph_data)
+
+    return render_template('dashboard.html', pie_data=pie_data, graph_data=graph_data)
+
 
 
 if __name__ == '__main__':
